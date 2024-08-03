310df317484f8fafd5eb9bb0750136e0dbf38022
Dang Van Nghiem <vannghiem848@gmail.com>
2024-08-03 22:02:18 +0700

    feat: cancel migrate http endpoint and update pyo3 asyncio

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 mod cache;
-mod endpoint;
 use pyo3::prelude::*;
 
 
 #[pymodule]
-fn pyfast(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn pyfast(_py: Python, m: &PyModule) -> PyResult<()>  {
 
-    let cache_module = PyModule::new_bound(m.py(), "cache")?;
+    let cache_module = PyModule::new(m.py(), "cache")?;
     cache_module.add_class::<cache::backend::BaseBackend>()?;
     cache_module.add_class::<cache::redis_backend::RedisBackend>()?;
 
