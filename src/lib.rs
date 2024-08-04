ef3fa551c44313cd9c9ac60eda4078227a08be6b
Dang Van Nghiem <vannghiem848@gmail.com>
2024-08-04 09:59:18 +0700

    feat: config cache module

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,11 @@ use pyo3::prelude::*;
 
 
 #[pymodule]
-fn pyfast(py: Python<'_>, m: &PyModule) -> PyResult<()>  {
+fn pyfast(_py: Python<'_>, m: &PyModule) -> PyResult<()>  {
 
-    let cache_module = PyModule::new(py, "cache")?;
-    cache_module.add_class::<cache::backend::BaseBackend>()?;
-    cache_module.add_class::<cache::redis_backend::RedisBackend>()?;
+    m.add_class::<cache::backend::BaseBackend>()?;
+    m.add_class::<cache::redis_backend::RedisBackend>()?;
 
-    m.add_submodule(&cache_module)?;
     m.add_class::<openapi::schemas::BaseSchemaGenerator>()?;
     m.add_class::<openapi::swagger::SwaggerUI>()?;    
 
