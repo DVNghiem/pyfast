524238d2ea62e45c79a7fa4f715ed00a0f6d27f6
Dang Van Nghiem <vannghiem848@gmail.com>
2024-12-10 00:56:43 +0700

    feat: update version to 0.3.2 and add QueuedHTTPEndpoint for request queuing

--- hypern/routing/__init__.py
+++ hypern/routing/__init__.py
@@ -1,4 +1,5 @@
 from .route import Route
 from .endpoint import HTTPEndpoint
+from .queue import QueuedHTTPEndpoint
 
-__all__ = ["Route", "HTTPEndpoint"]
+__all__ = ["Route", "HTTPEndpoint", "QueuedHTTPEndpoint"]
